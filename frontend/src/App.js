1efb095c283515dd306933f64c961c5bcc01475d
Walter Lozano Alcalde <walterlozanoalcalde@gmail.com>
2026-01-06 14:27:23 -0500

    Corregir nombre de tabla a 'config', agregar estructura PWA (iconos, favicon, manifest), instalar dependencias backend, crear archivos base React

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Aula Virtual</h1>
+        <p>Sistema de aula virtual</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
